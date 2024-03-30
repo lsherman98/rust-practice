012b0946135d62a7efd9c49a4225616fe2ca318f
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-03-30 12:03:59 -0400

    formatting in rust

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,36 @@
-use core::num;
-use rand::Rng;
-use std::{cmp::Ordering, io};
+use core::fmt;
+#[derive(Debug)]
+enum Gem {
+    Diamond,
+    Sapphire,
+    Ruby,
+    Topaz,
+    Onyx,
+    Jade,
+}
+
+impl fmt::Display for Gem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Gem::Diamond => write!(f, "diamond"),
+            Gem::Sapphire => write!(f, "sapphire"),
+            Gem::Ruby => write!(f, "ruby"),
+            Gem::Topaz => write!(f, "topaz"),
+            Gem::Onyx => write!(f, "onyx"),
+            Gem::Jade => write!(f, "jade"),
+        }
+    }
+}
 
 fn main() {
-    let mut how_many = String::new();
-    println!("How many numbers do you want to guess?");
-    io::stdin()
-        .read_line(&mut how_many)
-        .expect("error reading input");
+    let gems = [
+        (Gem::Onyx, 25.00),
+        (Gem::Diamond, 100.00),
+        (Gem::Onyx, 50.00),
+        (Gem::Ruby, 10.00),
+    ];
 
-    let num_guesses: u8 = how_many.trim().parse().expect("error reading input");
-
-    let mut correct = Vec::new();
-
-    for _ in 0..num_guesses {
-        correct.push(rand::thread_rng().gen_range(1..=10));
-    }
-
-    // println!("{correct:?}");
-
-    let mut guesses_made = 0;
-
-    while guesses_made < num_guesses {
-        println!("Guess a number: ");
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("error reading input");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(e) => {
-                println!("Error with parse, try again");
-                continue;
-            }
-        };
-
-        match guess.cmp(&correct[guesses_made as usize]) {
-            Ordering::Greater => println!("You guessed too high"),
-            Ordering::Less => println!("You guessed too low"),
-            Ordering::Equal => {
-                println!("You guessed the correct number");
-                guesses_made += 1;
-                if guesses_made < num_guesses {
-                    println!("Lets now try the next number!");
-                }
-            }
-        };
-    }
-    println!("The correct answers were:");
-    for item in correct {
-        println!("{item")
+    for gem in gems {
+        println!("this {:?} is worth {}", gem.0, gem.1);
     }
 }
