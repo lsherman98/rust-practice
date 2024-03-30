1a4d28e6ea0410e832af90843b5b96d2ce2d6c48
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-03-29 21:40:36 -0400

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use core::num;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    let correct = rand::thread_rng().gen_range(1..=10);
+    // println!("correct: {correct}");
+
+    loop {
+        println!("Guess a number: ");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error reading input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Error with parse, try again");
+                continue;
+            }
+        };
+
+        match guess.cmp(&correct) {
+            Ordering::Greater => println!("You guessed too high"),
+            Ordering::Less => println!("You guessed too low"),
+            Ordering::Equal => {
+                println!("You guessed the correct number");
+                break;
+            }
+        };
+    }
 }
